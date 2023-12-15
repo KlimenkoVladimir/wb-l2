72c102d76c2d9c6194effad0e9aa62dee19c8094
Vladimir_Klimenko <ownertheworld@yandex.ru>
2023-12-15 19:17:50 +0300

    feat: add reset

--- guess-number/index.js
+++ guess-number/index.js
@@ -4,9 +4,13 @@ let startNumber;
 let endNumber;
 const chooseElement = document.querySelector(".chooseRange");
 const guessElement = document.querySelector(".guessNumber");
+const resultElement = document.getElementById("result");
+const startInput = document.getElementById("startInput");
+const endInput = document.getElementById("endInput");
+const guessInput = document.getElementById("guess");
 
 function startGame() {
-  // Задаем число
+  // Задаем случайное число
   targetNumber =
     Math.floor(Math.random() * (endNumber - startNumber + 1)) + startNumber;
   attempts = 0;
@@ -20,7 +24,6 @@ function startGame() {
 }
 
 function checkGuess() {
-  const guessInput = document.getElementById("guess");
   const userGuess = parseInt(guessInput.value);
   console.log(targetNumber, startNumber, endNumber);
 
@@ -33,9 +36,7 @@ function checkGuess() {
   attempts++;
 
   if (userGuess === targetNumber) {
-    document.getElementById(
-      "result"
-    ).innerText = `Поздравляем! Вы угадали число за ${formatNumberWithEnding(
+    resultElement.innerHTML = `Поздравляем! Вы угадали число за ${formatNumberWithEnding(
       attempts,
       ["попытку", "попытки", "попыток"]
     )}.`;
@@ -48,10 +49,17 @@ function checkGuess() {
   }
 }
 
-function setDiapason() {
-  const startInput = document.getElementById("startInput");
-  const endInput = document.getElementById("endInput");
+function resetGame() {
+  startInput.value = "";
+  endInput.value = "";
+  guessInput.value = "";
+  resultElement.innerHTML = "";
+  chooseElement.style.display = "flex";
+  guessElement.style.display = "none";
+}
 
+// Задаем диапазон
+function setDiapason() {
   const start = parseInt(startInput.value);
   const end = parseInt(endInput.value);
   if (start && end && start < end) {
@@ -68,13 +76,13 @@ function getWordEnding(number, forms) {
   const remainder100 = number % 100;
 
   if (remainder10 === 1 && remainder100 !== 11) {
-    // Возвращаем первую форму если число заканчивается на 1 и последние 2 цифры у чисел больше 100 не 11
+    // Возвращаем первую форму если число заканчивается на 1 и последние 2 цифры (у чисел больше 100) не 11
     return forms[0];
   } else if (
     [2, 3, 4].includes(remainder10) &&
     ![12, 13, 14].includes(remainder100)
   ) {
-    // Возвращаем первую форму если число заканчивается на 2, 3, 4 и последние 2 цифры у чисел больше 100 не 12, 13, 14
+    // Возвращаем вторую форму если число заканчивается на 2, 3, 4 и последние 2 цифры (у чисел больше 100) не 12, 13, 14
     return forms[1];
   } else {
     // Возвращаем третью форму для всех остальных случаев
